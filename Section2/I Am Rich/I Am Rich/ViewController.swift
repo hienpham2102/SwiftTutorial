5e8976f5e060ed8054d3fc7a549c34ded7644ad1
hienpham2102 <hienpham2102@gmail.com>
2020-08-09 23:22:51 +0700

    XcodeStory and interface builder

---
+++ Section2/I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Macbook_Autonomous on 8/9/20.
+//  Copyright © 2020 Macbook_Autonomous. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
