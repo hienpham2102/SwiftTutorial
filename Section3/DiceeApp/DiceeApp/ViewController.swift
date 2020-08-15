5b02b5d384c863e4e6664a9ee11d1fe9fc483ec6
phamhien2102 <phamhien2102@gmail.com>
2020-08-15 16:25:36 +0700

    roll dices - done section 2,3,4

---
+++ Section3/DiceeApp/DiceeApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  DiceeApp
+//
+//  Created by Hien Pham on 8/15/20.
+//  Copyright © 2020 Hien Pham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func rollButtonPressed(_ sender: Any) {
+        diceImageView1.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][Int.random(in: 0...5)]
+        diceImageView2.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][Int.random(in: 0...5)]
+    }
+    
+
+}
+
